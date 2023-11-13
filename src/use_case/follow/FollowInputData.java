29c451c42a2118399256d1cca78fe6b4e9057854
Terry Fu <terry.fu@mail.utoronto.ca>
2023-11-13 17:41:32 -0500

    Finished FollowInputData.java

--- src/use_case/follow/FollowInputData.java
+++ src/use_case/follow/FollowInputData.java
@@ -1,4 +1,20 @@
 package use_case.follow;
+import entity.User;
 
 public class FollowInputData {
+    final private User follower;
+    final private User userBeingFollowed;
+
+    public FollowInputData(User follower, User userBeingFollowed) {
+        this.follower = follower;
+        this.userBeingFollowed = userBeingFollowed;
+    }
+
+    public User getFollower() {
+        return follower;
+    }
+
+    public User getUserBeingFollowed() {
+        return userBeingFollowed;
+    }
 }
