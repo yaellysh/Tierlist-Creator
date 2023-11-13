26625baee6678a07e35179a063b0ccee705ec768
Yael Lyshkow <yaellyshkow@outlook.com>
2023-11-13 18:53:26 -0500

    made junit tests for drop down

--- src/test/java/TierListTest.java
+++ src/test/java/TierListTest.java
@@ -46,49 +46,47 @@ public class TierListTest {
                 .equals(Tier.S);
     }
 
-
-    public class TierListTest {
-        // TODO add test case for home button once implemented
-        private static Tier getTierList() throws IOException {
-            TierListDataAccessInterface object = new FileUserDataAccessObject("src/main/resources/users.json");
-            return object.getUser("Yael").getTierList("Test").getItem("Item 1").getTier();
-        }
-
-        public JComboBox getDropDown() {
-            JFrame app = null;
-            Window[] windows = Window.getWindows();
-            for (Window window : windows) {
-                if (window instanceof JFrame) {
-                    app = (JFrame) window;
-                }
-            }
-            assertNotNull(app);
-            Component root = app.getComponent(0);
-            Component cp = ((JRootPane) root).getContentPane();
-            JPanel jp = (JPanel) cp;
-            JPanel jp2 = (JPanel) jp.getComponent(0);
-
-            TierListView sv = (TierListView) jp2.getComponent(0);
-
-            return (JComboBox) sv
-                    .getComponent(3)
-                    .getComponentAt(0, 0)
-                    .getComponentAt(73, 0)
-                    .getComponentAt(89, 5);
-        }
-
-        @Test
-        public void checkDropDown() throws IOException, InterruptedException {
-            Main.main(null);
-            Tier initialTier = getTierList();
-            assert initialTier.equals(Tier.S);
-            JComboBox dropDown = getDropDown();
-            Thread.sleep(1000);
-            dropDown.setSelectedItem("A");
-            Thread.sleep(1000);
-
-            Tier updatedTier = getTierList();
-            System.out.println(updatedTier);
-            assert updatedTier.equals(Tier.A);
-        }
+    // TODO add test case for home button once implemented
+    private static Tier getTierList() throws IOException {
+        TierListDataAccessInterface object = new FileUserDataAccessObject("src/main/resources/users.json");
+        return object.getUser("Yael").getTierList("Test").getItem("Item 1").getTier();
     }
+
+    public JComboBox getDropDown() {
+        JFrame app = null;
+        Window[] windows = Window.getWindows();
+        for (Window window : windows) {
+            if (window instanceof JFrame) {
+                app = (JFrame) window;
+            }
+        }
+        assertNotNull(app);
+        Component root = app.getComponent(0);
+        Component cp = ((JRootPane) root).getContentPane();
+        JPanel jp = (JPanel) cp;
+        JPanel jp2 = (JPanel) jp.getComponent(0);
+
+        TierListView sv = (TierListView) jp2.getComponent(0);
+
+        return (JComboBox) sv
+                .getComponent(3)
+                .getComponentAt(0, 0)
+                .getComponentAt(73, 0)
+                .getComponentAt(89, 5);
+    }
+
+    @Test
+    public void checkDropDown() throws IOException, InterruptedException {
+        Main.main(null);
+        Tier initialTier = getTierList();
+        assert initialTier.equals(Tier.S);
+        JComboBox dropDown = getDropDown();
+        Thread.sleep(1000);
+        dropDown.setSelectedItem("A");
+        Thread.sleep(1000);
+
+        Tier updatedTier = getTierList();
+        System.out.println(updatedTier);
+        assert updatedTier.equals(Tier.A);
+    }
+}
