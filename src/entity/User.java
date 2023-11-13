67a19e2680164201a17dd441c921a33b2e43040f
tianachan <107435880+tianachan@users.noreply.github.com>
2023-11-13 18:33:00 -0500

    added password to User

--- src/entity/User.java
+++ src/entity/User.java
@@ -5,14 +5,20 @@ import java.util.List;
 public class User {
 
     private final String username;
+    private final String password;
     private List<TierList> tierLists;
 
-    public User(String username) {
+    public User(String username, String password) {
         this.username = username;
+        this.password = password;
     }
 
     public String getUsername() {
         return username;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public List<TierList> getTierLists() {
