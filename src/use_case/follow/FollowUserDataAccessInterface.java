089e1c39d49225738d86633c054b77b2a0456563
Terry Fu <terry.fu@mail.utoronto.ca>
2023-11-13 18:00:12 -0500

    Finished FollowUserDataAccessInterface.java

--- src/use_case/follow/FollowUserDataAccessInterface.java
+++ src/use_case/follow/FollowUserDataAccessInterface.java
@@ -1,4 +1,8 @@
 package use_case.follow;
 
+import entity.User;
+
 public interface FollowUserDataAccessInterface {
+    void updateFollowing(User follower, User userBeingFollowed);
+    void updateUserBeingFollowed(User follower, User userBeingFollowed);
 }
