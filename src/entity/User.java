640bcf867d438ffef67770859dc5e0a3e3b95a35
Terry Fu <terry.fu@mail.utoronto.ca>
2023-11-14 14:47:00 -0500

    Updated the USer entity with content from other branches and added the get methods for followers and following

--- src/entity/User.java
+++ src/entity/User.java
@@ -1,18 +1,23 @@
 package entity;
 
-import java.util.List;
+        import java.util.ArrayList;
+        import java.util.HashMap;
+        import java.util.List;
+        import java.util.Map;
 
 public class User {
 
     private final String username;
-    private List<TierList> tierLists;
-
+    private final Map<String, TierList> tierLists;
     private List<TierList> liked;
     private List<User> following;
     private List<User> followers;
 
     public User(String username) {
         this.username = username;
+        this.tierLists = new HashMap<>();
+        this.following = new ArrayList<>();
+        this.followers = new ArrayList<>();
     }
 
     public String getUsername() {
@@ -20,10 +25,28 @@ public class User {
     }
 
     public List<TierList> getTierLists() {
-        return tierLists;
+        return new ArrayList<>(tierLists.values());
     }
 
-    public void setTierLists(List<TierList> tierLists) {
-        this.tierLists = tierLists;
+//    public void addTierList(TierList list) {
+//        this.tierLists.put(list.getName(), list);
+//    }
+
+    public TierList getTierList(String name) {
+        return this.tierLists.get(name);
+    }
+
+    @Override
+    public String toString() {
+        return this.username;
+    }
+
+    public List<User> getFollowing() {
+        return new ArrayList<>(following);
+    }
+
+    public List<User> getFollowers() {
+        return new ArrayList<>(followers);
     }
 }
+
