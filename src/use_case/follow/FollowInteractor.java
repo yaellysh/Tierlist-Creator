b212bcc65f472cb11812a03c4e1f0f10c260241b
Terry Fu <terry.fu@mail.utoronto.ca>
2023-11-14 01:08:50 -0500

    Added the attribute variables and constructor for FollowInteractor.java

--- src/use_case/follow/FollowInteractor.java
+++ src/use_case/follow/FollowInteractor.java
@@ -1,4 +1,14 @@
 package use_case.follow;
 
 public class FollowInteractor {
+    final FollowUserDataAccessInterface userDataAccessObject;
+    final FollowOutputBoundary followPresenter;
+
+    public FollowInteractor(FollowUserDataAccessInterface userDataAccessInterface,
+                            FollowOutputBoundary loginOutputBoundary) {
+        userDataAccessObject = userDataAccessInterface;
+        followPresenter = loginOutputBoundary;
+    }
+
+    public void execute
 }
