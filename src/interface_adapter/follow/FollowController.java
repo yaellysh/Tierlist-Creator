fbb00be12fd1d4f7922a3c8581201d512b7e9032
Terry Fu <terry.fu@mail.utoronto.ca>
2023-11-15 01:01:30 -0500

    Finished FollowController.java, may need to adjust input from User to String

--- src/interface_adapter/follow/FollowController.java
+++ src/interface_adapter/follow/FollowController.java
@@ -1,4 +1,18 @@
 package interface_adapter.follow;
 
+import entity.User;
+import use_case.follow.FollowInputBoundary;
+import use_case.follow.FollowInputData;
+
 public class FollowController {
+    final FollowInputBoundary followUseCaseInteractor;
+
+    public FollowController(FollowInputBoundary followInputBoundary) {
+        followUseCaseInteractor = followInputBoundary;
+    }
+
+    public void execute(User follower, User userBeingFollowed) {
+        FollowInputData inputData = new FollowInputData(follower, userBeingFollowed);
+        followUseCaseInteractor.execute(inputData);
+    }
 }
