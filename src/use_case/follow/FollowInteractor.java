e26ebf884c9fa5c5cd1e8ccd4f06236cfecba7a8
Terry Fu <terry.fu@mail.utoronto.ca>
2023-11-17 14:43:10 -0500

    started on the for loop to find related users, will have to modify my input data before continuing

--- src/use_case/follow/FollowInteractor.java
+++ src/use_case/follow/FollowInteractor.java
@@ -2,6 +2,8 @@ package use_case.follow;
 
 import entity.User;
 
+import java.util.List;
+
 public class FollowInteractor implements FollowInputBoundary {
     final FollowUserDataAccessInterface userDataAccessObject;
     final FollowOutputBoundary followPresenter;
@@ -15,8 +17,19 @@ public class FollowInteractor implements FollowInputBoundary {
     public void execute(FollowInputData followInputData) {
         User follower = followInputData.getFollower();
         User userBeingFollowed = followInputData.getUserBeingFollowed();
+
+
+
         userDataAccessObject.updateFollowing(follower, userBeingFollowed);
         userDataAccessObject.updateUserBeingFollowed(follower, userBeingFollowed);
+        // want to find the users that most of my following also follow
+        // "for example, people you know also follow ..."
+        List<User> followerFollowing = follower.getFollowing();
+        List<User> userBeingFollowedFollowers = userBeingFollowed.getFollowers();
+
+        for (User followerFollower : followerFollowing) {
+
+        }
 
 
     }
