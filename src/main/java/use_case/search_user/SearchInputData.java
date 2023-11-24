650e87eef221fe2e29a0fd3ac89530758298211f
Terry Fu <terry.fu@mail.utoronto.ca>
2023-11-24 18:13:07 -0500

    finished the search and view user input boundary

--- src/main/java/use_case/search_user/SearchInputData.java
+++ src/main/java/use_case/search_user/SearchInputData.java
@@ -1,4 +1,5 @@
 package use_case.search_user;
 
 public class SearchInputData {
+    private final
 }
--- src/main/java/use_case/view_user/ViewUserInputBoundary.java
+++ src/main/java/use_case/view_user/ViewUserInputBoundary.java
@@ -1,4 +1,5 @@
 package use_case.view_user;
 
 public interface ViewUserInputBoundary {
+    void execute(ViewUserInputData viewUserInputData);
 }
