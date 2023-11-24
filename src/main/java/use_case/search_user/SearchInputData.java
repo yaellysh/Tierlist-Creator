f510485a370c9ea6ff0d3279c8e12b3aa25a0d93
Terry Fu <terry.fu@mail.utoronto.ca>
2023-11-24 18:30:56 -0500

    finished the search and view user input data

--- src/main/java/use_case/search_user/SearchInputData.java
+++ src/main/java/use_case/search_user/SearchInputData.java
@@ -1,5 +1,14 @@
 package use_case.search_user;
 
 public class SearchInputData {
-    private final
+    private final String search;
+
+    public SearchInputData(String search) {
+        this.search = search;
+    }
+
+    public String getSearch() {
+        return search;
+    }
+
 }
