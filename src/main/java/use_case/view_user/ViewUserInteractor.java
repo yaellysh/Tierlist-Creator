c5606753350f00339aa37a2d33aecb4524db265e
Terry Fu <terry.fu@mail.utoronto.ca>
2023-11-24 22:10:35 -0500

    Finished the view user interactor

--- src/main/java/use_case/view_user/ViewUserInteractor.java
+++ src/main/java/use_case/view_user/ViewUserInteractor.java
@@ -1,5 +1,9 @@
 package use_case.view_user;
 
+import entity.User;
+
+import java.util.List;
+
 public class ViewUserInteractor implements ViewUserInputBoundary {
     final ViewUserDataAccessInterface userDataAccessObject;
     final ViewUserOutputBoundary viewUserPresenter;
@@ -11,6 +15,15 @@ public class ViewUserInteractor implements ViewUserInputBoundary {
     }
 
     public void execute(ViewUserInputData viewUserInputData) {
+        // get the selected user from input data
+        String selectedUsername = viewUserInputData.getSelectedUser();
+        User selectedUser = userDataAccessObject.getUser(selectedUsername);
 
+        int followerCount = selectedUser.getFollowers().size();
+        int followingCount = selectedUser.getFollowing().size();
+        List<String> tierLists = selectedUser.getTierLists();
+
+        ViewUserOutputData viewUserOutputData = new ViewUserOutputData(tierLists, followerCount, followingCount);
+        viewUserPresenter.prepareSuccessView(viewUserOutputData);
     }
 }
