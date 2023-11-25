e235a0f1ecd640bd3d1101fa5d6261ba912be6e9
Terry Fu <terry.fu@mail.utoronto.ca>
2023-11-24 20:08:29 -0500

    Finished Search and View user output boundaries

--- src/main/java/use_case/search_user/SearchOutputBoundary.java
+++ src/main/java/use_case/search_user/SearchOutputBoundary.java
@@ -1,5 +1,6 @@
 package use_case.search_user;
 
 public interface SearchOutputBoundary {
-    void prepareSuccessView()
+    void prepareSuccessView(SearchOutputData searchOutputData);
+    void prepareFailView(String error);
 }
