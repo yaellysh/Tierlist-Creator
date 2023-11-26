531ae4ad043e9bcf0572f59b8cbfd7c8c14fea50
Terry Fu <terry.fu@mail.utoronto.ca>
2023-11-25 19:20:42 -0500

    Added input data for the state of the follow button

--- src/main/java/use_case/follow/FollowInputData.java
+++ src/main/java/use_case/follow/FollowInputData.java
@@ -2,12 +2,14 @@ package use_case.follow;
 import entity.User;
 
 public class FollowInputData {
+    final private boolean follow;
     final private String followerName;
     final private String userBeingFollowedName;
 
-    public FollowInputData(String follower, String userBeingFollowed) {
+    public FollowInputData(String follower, String userBeingFollowed, boolean follow) {
         this.followerName = follower;
         this.userBeingFollowedName = userBeingFollowed;
+        this.follow = follow;
     }
 
     public String getFollower() {
