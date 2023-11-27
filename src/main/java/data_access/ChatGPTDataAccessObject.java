f84a106270b5ebde33894e9909c95fefed1ab3b6
EltrutLikes <gesc808@gmail.com>
2023-11-27 18:55:36 -0500

    implement chatgpt

--- src/main/java/data_access/ChatGPTDataAccessObject.java
+++ src/main/java/data_access/ChatGPTDataAccessObject.java
@@ -1,6 +1,7 @@
 package data_access;
 
 import entity.Item;
+import interface_adapter.tierlist.TierListViewModel;
 import use_case.generate.random_tierlist.RandomTierListDataAccessInterface;
 
 import java.io.BufferedReader;
@@ -9,51 +10,47 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Map;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ChatGPTDataAccessObject implements RandomTierListDataAccessInterface {
 
     public static String chatGPT(String prompt) throws IOException {
 
-    // https://rollbar.com/blog/how-to-use-chatgpt-api-with-java/ this website was used to help integrate the ChatGPT
-    // API with Java
+        // https://rollbar.com/blog/how-to-use-chatgpt-api-with-java/ this website was used to help integrate the ChatGPT
+        // API with Java
 
         String apiKey = System.getenv("OPENAI_API_KEY");
         String url = "https://api.openai.com/v1/chat/completions";
         String model = "gpt-3.5-turbo";
 
-        try {
-            URL obj = new URL(url);
-            HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Authorization", "Bearer " + apiKey);
-            connection.setRequestProperty("Content-Type", "application/json");
+        URL obj = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Authorization", "Bearer " + apiKey);
+        connection.setRequestProperty("Content-Type", "application/json");
 
-            // The request body
-            String body = "{\"model\": \"" + model + "\", \"messages\": [{\"role\": \"user\", \"content\": \"" + prompt + "\"}]}";
-            connection.setDoOutput(true);
-            OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
-            writer.write(body);
-            writer.flush();
-            writer.close();
+        // The request body
+        String body = "{\"model\": \"" + model + "\", \"messages\": [{\"role\": \"user\", \"content\": \"" + prompt + "\"}]}";
+        connection.setDoOutput(true);
+        OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+        writer.write(body);
+        writer.flush();
+        writer.close();
 
-            // Response from ChatGPT
-            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String line;
+        // Response from ChatGPT
+        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String line;
 
-            StringBuilder response = new StringBuilder();
+        StringBuilder response = new StringBuilder();
 
-            while ((line = br.readLine()) != null) {
-                response.append(line);
-            }
-            br.close();
-            System.out.println(response.toString());
-            // calls the method to extract the message.
-            return extractMessageFromJSONResponse(response.toString());
-
-        } catch (IOException e) {
-            throw new RuntimeException(e); // I DONT WANT THIS RN
+        while ((line = br.readLine()) != null) {
+            response.append(line);
         }
+        br.close();
+        System.out.println(response.toString());
+        // calls the method to extract the message.
+        return extractMessageFromJSONResponse(response.toString());
     }
 
     public static String extractMessageFromJSONResponse(String response) {
@@ -67,8 +64,19 @@ public class ChatGPTDataAccessObject implements RandomTierListDataAccessInterfac
 
 
     @Override
-    public Map<String, Item> generateTierList(String prompt) {
-        return null;
+    public List<Item> generateTierList(String prompt) {
+        try {
+            String result = chatGPT(prompt);
+            List<String> list = List.of(result.split("\n"));
+
+            if (list.size() != TierListViewModel.NUM_ITEMS) {
+                return null;
+            }
+            return list.stream().map(Item::new).collect(Collectors.toList());
+        }
+        catch (IOException e) {
+            return null;
+        }
     }
 }
 
--- src/main/java/use_case/generate/random_tierlist/RandomTierListInteractor.java
+++ src/main/java/use_case/generate/random_tierlist/RandomTierListInteractor.java
@@ -1,16 +1,40 @@
 package use_case.generate.random_tierlist;
 
+import entity.Item;
+import entity.TierList;
+import entity.User;
+import use_case.generate.GenerateTierListDataAccessInterface;
+
+import java.util.List;
+
 public class RandomTierListInteractor implements RandomTierListInputBoundary {
 
+    private final RandomTierListDataAccessInterface randomTierListDataAccessInterface;
+    private final GenerateTierListDataAccessInterface generateTierListDataAccessInterface;
     private final RandomTierListOutputBoundary outputBoundary;
 
-    public RandomTierListInteractor(RandomTierListOutputBoundary outputBoundary) {
+    public RandomTierListInteractor(RandomTierListDataAccessInterface dataAccessInterface,
+                                    GenerateTierListDataAccessInterface dataAccessInterface1,
+                                    RandomTierListOutputBoundary outputBoundary) {
+        this.randomTierListDataAccessInterface = dataAccessInterface;
+        this.generateTierListDataAccessInterface = dataAccessInterface1;
         this.outputBoundary = outputBoundary;
     }
     @Override
     public void execute(RandomTierListInputData data) {
-        // Generate the tier list
-        // Make sure it's right
+        String prompt = data.getPrompt();
+        User user = data.getUser();
+        List<Item> items = randomTierListDataAccessInterface.generateTierList(prompt);
+
+        if (items == null) {
+            outputBoundary.prepareFailView();
+            return;
+        }
+
+        TierList list = new TierList(prompt, items);
+        generateTierListDataAccessInterface.addTierList(user, list);
+        outputBoundary.prepareSuccessView(new RandomTierListOutputData(user, prompt));
+
         // Pass it on.
     }
 }
