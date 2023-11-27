e78e9a28eabd2d5a7c4c35cf49d8bc6ae3ecf663
tianachan <107435880+tianachan@users.noreply.github.com>
2023-11-27 18:08:13 -0500

    fixed bugs, code now runs

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,6 +1,7 @@
 package app;
 
 import data_access.FileUserDataAccessObject;
+import entity.UserFactory;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.logged_in.LoggedInViewModel;
 import interface_adapter.login.LoginViewModel;
@@ -43,7 +44,7 @@ public class Main {
 
         FileUserDataAccessObject userDataAccessObject;
         try {
-            userDataAccessObject = new FileUserDataAccessObject("./users.csv", new CommonUserFactory());
+            userDataAccessObject = new FileUserDataAccessObject("./users.csv", new UserFactory());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
