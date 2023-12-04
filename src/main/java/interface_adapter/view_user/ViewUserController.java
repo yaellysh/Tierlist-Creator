168939ad33cc1c6055050983fc541a818387828d
Terry Fu <terry.fu@mail.utoronto.ca>
2023-12-04 16:56:00 -0500

    Created and started on ViewUserController

---
+++ src/main/java/interface_adapter/view_user/ViewUserController.java
@@ -0,0 +1,7 @@
+package interface_adapter.view_user;
+
+import use_case.view_user.ViewUserInputBoundary;
+
+public class ViewUserController {
+    final ViewUserInputBoundary viewUserUseCaseInteractor;
+}
