12a0b7c1c254d8afc59891a4a6c59f85f3705929
Heian0 <77514857+Heian0@users.noreply.github.com>
2023-12-04 13:11:09 -0800

    frontend for search. Edited follow frontend

---
+++ src/main/java/interface_adapter/search_user/SearchState.java
@@ -0,0 +1,13 @@
+package interface_adapter.search_user;
+
+public class SearchState {
+    private String search;
+
+    public String getSearch() {
+        return search;
+    }
+
+    public void setSearch(String search) {
+        this.search = search;
+    }
+}
