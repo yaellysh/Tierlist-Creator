512f64a28bd72ed346c6dfe9c186f3c9283994ee
Terry Fu <terry.fu@mail.utoronto.ca>
2023-12-04 16:21:21 -0500

    more tests

--- test/use_case/view_user/ViewUserInteractorTest.java
+++ test/use_case/view_user/ViewUserInteractorTest.java
@@ -1,7 +1,16 @@
 package use_case.view_user;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ViewUserInteractorTest {
+
+
+
+    @Test
+    void successTest() {
+
+    }
 
 }
