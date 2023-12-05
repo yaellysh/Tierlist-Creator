2054575c83d7cbe365e7748da153a612774b9929
Yael Lyshkow <yaellyshkow@outlook.com>
2023-12-05 10:57:51 -0500

    added search uses button to home screen, just need to link it to terry tim use case classes and screens once merged

--- src/main/java/interface_adapter/selector/SelectorViewModel.java
+++ src/main/java/interface_adapter/selector/SelectorViewModel.java
@@ -11,9 +11,10 @@ import java.util.List;
 public class SelectorViewModel extends ViewModel {
 
     public static final String TITLE_LABEL = "Tier List Selector";
+    public static final String SEARCH_INSTRUCTIONS = "You can <b>SEARCH AND FOLLOW OTHER USERS</b> and view their tierlists below:";
     public static final String INSTRUCTIONS1 = "Selecting <b>VIEW EXISTING</b> will allow you to choose from the tier lists you have already sorted, edit them and save these changes if you wish.";
     public static final String INSTRUCTIONS2 = "Selecting the <b>RANDOM</b> option will allow you to enter a topic of your choosing. From this prompt a tierlist will then be automatically generated for you to organise.<br><br> Selecting the <b>CUSTOM</b> option will allow you to enter the items you want to place in your tier list. You will be required to type in 10 different items which you can then rank.";
-    public static final List<Color> COLORS = Arrays.asList(Color.red, Color.blue, Color.green);
+    public static final String SEARCH_BUTTON = "Search Users";
     public static final List<String> BUTTONS = Arrays.asList("View Existing", "Random", "Custom");
     public static final Font TITLE_FONT = new Font("Arial Bold", Font.PLAIN, 30);
     public static final Font BUTTON_FONT = new Font("Arial", Font.ITALIC, 25);
