a6bc5532c789ff4ab24af4fcb5159f19f679da75
Yael Lyshkow <137076627+yaellysh@users.noreply.github.com>
2023-12-06 18:48:54 -0500

    made test for selector buttons

--- src/test/java/TierListTest.java
+++ src/test/java/TierListTest.java
@@ -1,12 +1,36 @@
-import factory.SignupFactory;
+import data_access.ChatGPTDataAccessObject;
+import data_access.FileUserDataAccessObject;
+import entity.User;
+import factory.*;
+import interface_adapter.ViewManagerModel;
+import interface_adapter.custom_tierlist.CustomTierListState;
+import interface_adapter.custom_tierlist.CustomTierListViewModel;
+import interface_adapter.follow.FollowViewModel;
+import interface_adapter.login.LoginViewModel;
+import interface_adapter.menu.MenuViewModel;
+import interface_adapter.random_tierlist.RandomTierListState;
+import interface_adapter.random_tierlist.RandomTierListViewModel;
+import interface_adapter.selector.SelectorState;
+import interface_adapter.selector.SelectorViewModel;
+import interface_adapter.signup.SignupViewModel;
+import interface_adapter.tierlist.TierListState;
+import interface_adapter.tierlist.TierListViewModel;
+import interface_adapter.view_existing.ViewExistingState;
+import interface_adapter.view_existing.ViewExistingViewModel;
+import interface_adapter.view_user.ViewUserViewModel;
 import org.junit.Test;
 import view.*;
 
 import javax.swing.*;
+import javax.swing.text.View;
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Objects;
+import java.util.List;
 
 import static org.junit.Assert.assertNotNull;
+
 
 public class TierListTest {
 
@@ -33,8 +57,8 @@ public class TierListTest {
                 .getComponentAt(89, 5);
     }
 
-    public MenuView getMenuView() throws InterruptedException {
-        Main.main(null);
+    public Component getView(String viewName) throws InterruptedException {
+        miniMain();
 
         JFrame app = null;
         Window[] windows = Window.getWindows();
@@ -50,17 +74,26 @@ public class TierListTest {
         JPanel jp = (JPanel) cp;
         JPanel viewPanel = (JPanel) jp.getComponent(0);
 
-        MenuView menuView = null;
-
-        for (Component view : viewPanel.getComponents()) {
-            if (view instanceof MenuView) {
-                menuView = (MenuView) view;
+        for (Component component : viewPanel.getComponents()) {
+            if (Objects.equals(viewName, "menu")) {
+                if (component instanceof MenuView) {
+                    return component;
+                }
+            } else if (Objects.equals(viewName, "signup")) {
+                if (component instanceof SignupView) {
+                    return component;
+                }
+            } else if (Objects.equals(viewName, "selector")) {
+                if (component instanceof SelectorView) {
+                    return component;
+                }
             }
+
         }
-        return menuView;
+        return null;
     }
 
-    public Component getCurrentView() throws InterruptedException {
+    public Component getCurrentView() {
         Component currentView = null;
 
         JFrame app = null;
@@ -88,9 +121,9 @@ public class TierListTest {
 
     @Test
     public void checkMenuSignUpButton() throws InterruptedException {
-        Main.main(null);
+        miniMain();
 
-        MenuView menuView = getMenuView();
+        MenuView menuView = (MenuView) getView("menu");
         assert menuView != null;
         JPanel signUpWrapperPanel = (JPanel) menuView.getComponent(3);
         JPanel signUpPanel = (JPanel) signUpWrapperPanel.getComponent(0);
@@ -102,38 +135,38 @@ public class TierListTest {
         assert currentView instanceof SignupView;
     }
 
-    public SignupView getSignupView() throws InterruptedException {
-        Main.main(null);
-
-        JFrame app = null;
-        Window[] windows = Window.getWindows();
-        for (Window window : windows) {
-            if (window instanceof JFrame) {
-                app = (JFrame) window;
-            }
-        }
-
-        assertNotNull(app);
-        Component root = app.getComponent(0);
-        Component cp = ((JRootPane) root).getContentPane();
-        JPanel jp = (JPanel) cp;
-        JPanel viewPanel = (JPanel) jp.getComponent(0);
-
-        SignupView signupView = null;
-
-        for (Component view : viewPanel.getComponents()) {
-            if (view instanceof SignupView) {
-                signupView = (SignupView) view;
-            }
-        }
-        return signupView;
-    }
+//    public SignupView getSignupView() throws InterruptedException {
+//        miniMain();
+//
+//        JFrame app = null;
+//        Window[] windows = Window.getWindows();
+//        for (Window window : windows) {
+//            if (window instanceof JFrame) {
+//                app = (JFrame) window;
+//            }
+//        }
+//
+//        assertNotNull(app);
+//        Component root = app.getComponent(0);
+//        Component cp = ((JRootPane) root).getContentPane();
+//        JPanel jp = (JPanel) cp;
+//        JPanel viewPanel = (JPanel) jp.getComponent(0);
+//
+//        SignupView signupView = null;
+//
+//        for (Component view : viewPanel.getComponents()) {
+//            if (view instanceof SignupView) {
+//                signupView = (SignupView) view;
+//            }
+//        }
+//        return signupView;
+//    }
 
     @Test
     public void checkMenuLoginButton() throws InterruptedException {
-        Main.main(null);
+        miniMain();
 
-        MenuView menuView = getMenuView();
+        MenuView menuView = (MenuView) getView("menu");
         assert menuView != null;
 
         JPanel loginWrapperPanel = (JPanel) menuView.getComponent(7);
@@ -147,39 +180,67 @@ public class TierListTest {
         assert currentView instanceof LoginView;
     }
 
+//    @Test
+//    public void checkSignUp() throws InterruptedException {
+//        SignupView signupView = (SignupView) getView("signup");
+//
+//        InputPanel usernameInputPanel = (InputPanel) signupView.getComponent(1);
+//        JTextField usernameInput = usernameInputPanel.getTextField();
+//
+//        PasswordInputPanel passwordInputPanel = (PasswordInputPanel) signupView.getComponent(2);
+//        JTextField passwordInput = passwordInputPanel.getTextField();
+//
+//        PasswordInputPanel repeatPasswordInputPanel = (PasswordInputPanel) signupView.getComponent(3);
+//        JTextField repeatPasswordInput = repeatPasswordInputPanel.getTextField();
+//
+//        usernameInput.setText("Test");
+//        passwordInput.setText("Test");
+//        repeatPasswordInput.setText("Test");
+//
+//        JPanel buttonPanelWrapper = (JPanel) signupView.getComponent(4);
+//        ButtonPanel buttonPanel = (ButtonPanel) buttonPanelWrapper.getComponent(1);
+//        JButton signUpButton = buttonPanel.getButton();
+//
+//        signUpButton.doClick();
+//
+//    }
     @Test
-    public void checkSignUp() throws InterruptedException {
-        SignupView signupView = getSignupView();
+    public void checkSelectorButtons() throws InterruptedException {
+        SelectorView selectorView = (SelectorView) getView("selector");
+        assert selectorView != null;
 
-        InputPanel usernameInputPanel = (InputPanel) signupView.getComponent(1);
-        JTextField usernameInput = usernameInputPanel.getTextField();
+        List<JButton> buttons = new ArrayList<>();
 
-        PasswordInputPanel passwordInputPanel = (PasswordInputPanel) signupView.getComponent(2);
-        JTextField passwordInput = passwordInputPanel.getTextField();
+        for (Component component: selectorView.getComponents()) {
+            if (component instanceof ButtonPanel) {
+                buttons.add(((ButtonPanel) component).getButton());
+            }
+        }
+        for (JButton button: buttons) {
+            button.doClick();
+            if (button.getText().equals("View Existing")){
 
-        PasswordInputPanel repeatPasswordInputPanel = (PasswordInputPanel) signupView.getComponent(3);
-        JTextField repeatPasswordInput = repeatPasswordInputPanel.getTextField();
-
-        usernameInput.setText("Test");
-        passwordInput.setText("Test");
-        repeatPasswordInput.setText("Test");
-
-        JPanel buttonPanelWrapper = (JPanel) signupView.getComponent(4);
-        ButtonPanel buttonPanel = (ButtonPanel) buttonPanelWrapper.getComponent(1);
-        JButton signUpButton = buttonPanel.getButton();
-
-        signUpButton.doClick();
-
-    }
-
-    @Test
-    public void checkViewExistingTierlists() {
-
+                Component currentView = getCurrentView();
+                assert currentView instanceof ViewExistingView;
+            } else if (button.getText().equals("Random") ) {
+                Component currentView = getCurrentView();
+                assert currentView instanceof RandomTierListView;
+            } else if (button.getText().equals("Custom")) {
+                Component currentView = getCurrentView();
+                assert currentView instanceof CustomTierListView;
+            }
+        }
+        System.out.println(buttons);
     }
 
 //    @Test
+//    public void checkViewExistingTierlists() {
+//
+//    }
+
+//    @Test
 //    public void checkDropDown() throws IOException, InterruptedException {
-//        Main.main(null);
+//        miniMain();
 //        Tier initialTier = getTierList();
 //        assert initialTier.equals(Tier.S);
 //        JComboBox dropDown = getDropDown();
@@ -192,4 +253,96 @@ public class TierListTest {
 //        dropDown.setSelectedItem("S");
 //        Thread.sleep(100);
 //    }
+
+    public static void miniMain() throws InterruptedException {
+
+        JFrame application = new JFrame("Tierlist Maker");
+        application.setResizable(false);
+        application.setSize(800, 700);
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        CardLayout cardLayout = new CardLayout();
+        JPanel views = new JPanel(cardLayout);
+        application.add(views);
+
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        new ViewManager(views, cardLayout, viewManagerModel);
+
+        TierListViewModel tierListViewModel = new TierListViewModel("tier list");
+        SelectorViewModel selectorViewModel = new SelectorViewModel("selector");
+        RandomTierListViewModel randomTierListViewModel = new RandomTierListViewModel("random");
+        CustomTierListViewModel customTierListViewModel = new CustomTierListViewModel("custom");
+        ViewExistingViewModel viewExistingViewModel = new ViewExistingViewModel("view existing");
+        FollowViewModel followViewModel = new FollowViewModel("follow User");
+        ViewUserViewModel viewUserViewModel = new ViewUserViewModel("view User");
+        MenuViewModel menuViewModel = new MenuViewModel();
+        LoginViewModel loginViewModel = new LoginViewModel();
+        SignupViewModel signupViewModel = new SignupViewModel();
+
+        User user = new User("Test", "Password");
+
+        TierListState tierListState = tierListViewModel.getState();
+        tierListState.setUser(user);
+        tierListViewModel.setState(tierListState);
+
+        SelectorState selectorState = selectorViewModel.getState();
+        selectorState.setUser(user);
+        selectorViewModel.setState(selectorState);
+
+        RandomTierListState randomTierListState = randomTierListViewModel.getState();
+        randomTierListState.setUser(user);
+        randomTierListViewModel.setState(randomTierListState);
+        
+        CustomTierListState customTierListState = customTierListViewModel.getState();
+        customTierListState.setUser(user);
+        customTierListViewModel.setState(customTierListState);
+
+        ViewExistingState viewExistingState = viewExistingViewModel.getState();
+        viewExistingState.setUser(user);
+        viewExistingViewModel.setState(viewExistingState);
+
+        
+
+        FileUserDataAccessObject userDataAccessObject = new FileUserDataAccessObject("src/main/resources/users.json");
+        ChatGPTDataAccessObject chatGPTDataAccessObject  = new ChatGPTDataAccessObject();
+
+        RandomTierListView randomTierListView = RandomTierListFactory.create(viewManagerModel, randomTierListViewModel, userDataAccessObject, chatGPTDataAccessObject, tierListViewModel, selectorViewModel);
+
+        views.add(randomTierListView, randomTierListView.viewName);
+
+        TierListView tierListView = TierListFactory.create(viewManagerModel, tierListViewModel, userDataAccessObject, selectorViewModel);
+
+        MenuView menuView = MenuFactory.create(viewManagerModel, menuViewModel, loginViewModel, signupViewModel);
+        views.add(menuView, menuView.viewName);
+
+        SelectorView selectorView = SelectorFactory.create(viewManagerModel, selectorViewModel, randomTierListViewModel, customTierListViewModel, userDataAccessObject, viewExistingViewModel, menuViewModel);
+
+        views.add(selectorView, selectorView.viewName);
+
+        SignupView signupView = SignupFactory.create(viewManagerModel, loginViewModel, signupViewModel, userDataAccessObject);
+        views.add(signupView, signupView.viewName);
+
+        LoginView loginView = LoginFactory.create(viewManagerModel, loginViewModel, userDataAccessObject, selectorViewModel);
+        views.add(loginView, loginView.viewName);
+
+        ViewExistingView viewExistingView = ViewExistingFactory.create(viewManagerModel, viewExistingViewModel, tierListViewModel, selectorViewModel, userDataAccessObject);
+        views.add(viewExistingView, viewExistingView.viewName);
+
+        CustomTierListView customTierListView = CustomTierListFactory.create(viewManagerModel, customTierListViewModel, tierListViewModel, tierListView, selectorViewModel, userDataAccessObject);
+
+        views.add(customTierListView, customTierListView.viewName);
+        views.add(tierListView, tierListView.viewName);
+
+//        FollowState testing = new FollowState("terryfufu", "lt_rui", false);
+//        followViewModel.setState(testing);
+
+        FollowView followView = FollowFactory.create(viewManagerModel, followViewModel, viewUserViewModel, userDataAccessObject, userDataAccessObject);
+        views.add(followView, followView.viewName);
+
+        viewManagerModel.setActiveView(menuView.viewName);
+        viewManagerModel.firePropertyChanged();
+
+        application.setVisible(true);
+
+    }
 }
